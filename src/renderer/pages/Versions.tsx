48ccfd507b60a2012dec709004b968e16a273431
Jonathan Deiss <39541470+FireSquid6@users.noreply.github.com>
2023-02-05 18:56:19 -0600

    add fetch system

--- src/renderer/pages/Versions.tsx
+++ src/renderer/pages/Versions.tsx
@@ -1,3 +1,22 @@
 export default function Versions() {
-  return <h1>Versions page</h1>;
+  const handleClick = () => {
+    fetch(
+      'https://downloads.tuxfamily.org/godotengine/3.5/Godot_v3.5-stable_win64.exe.zip'
+    )
+      .then((response) => {
+        return console.log(response);
+      })
+      .catch((_error) => {
+        console.log('well crap');
+      });
+  };
+
+  return (
+    <>
+      <h1>Versions</h1>
+      <button type="button" onClick={handleClick}>
+        Click me!
+      </button>
+    </>
+  );
 }
