bcf75108c579354816abfb41d6567cd47757af05
Jonathan Deiss <39541470+FireSquid6@users.noreply.github.com>
2023-02-21 23:21:16 -0600

    add statusbar

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,9 +1,8 @@
-import { app, BrowserWindow, shell, ipcMain } from 'electron'
-import { release } from 'node:os'
-import { join } from 'node:path'
-import { download } from 'electron-dl'
-const Store = require('electron-store');
-
+import { app, BrowserWindow, shell, ipcMain } from "electron";
+import { release } from "node:os";
+import { join } from "node:path";
+import { download } from "electron-dl";
+const Store = require("electron-store");
 
 // The built directory structure
 //
@@ -15,21 +14,21 @@ const Store = require('electron-store');
 // ├─┬ dist
 // │ └── index.html    > Electron-Renderer
 //
-process.env.DIST_ELECTRON = join(__dirname, '../')
-process.env.DIST = join(process.env.DIST_ELECTRON, '../dist')
+process.env.DIST_ELECTRON = join(__dirname, "../");
+process.env.DIST = join(process.env.DIST_ELECTRON, "../dist");
 process.env.PUBLIC = process.env.VITE_DEV_SERVER_URL
-  ? join(process.env.DIST_ELECTRON, '../public')
-  : process.env.DIST
+  ? join(process.env.DIST_ELECTRON, "../public")
+  : process.env.DIST;
 
 // Disable GPU Acceleration for Windows 7
-if (release().startsWith('6.1')) app.disableHardwareAcceleration()
+if (release().startsWith("6.1")) app.disableHardwareAcceleration();
 
 // Set application name for Windows 10+ notifications
-if (process.platform === 'win32') app.setAppUserModelId(app.getName())
+if (process.platform === "win32") app.setAppUserModelId(app.getName());
 
 if (!app.requestSingleInstanceLock()) {
-  app.quit()
-  process.exit(0)
+  app.quit();
+  process.exit(0);
 }
 
 // Remove electron security warnings
@@ -37,17 +36,17 @@ if (!app.requestSingleInstanceLock()) {
 // Read more on https://www.electronjs.org/docs/latest/tutorial/security
 // process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
 
-let win: BrowserWindow | null = null
+let win: BrowserWindow | null = null;
 // Here, you can also use other preload
-const preload = join(__dirname, '../preload/index.js')
-const url = process.env.VITE_DEV_SERVER_URL
-const indexHtml = join(process.env.DIST, 'index.html')
+const preload = join(__dirname, "../preload/index.js");
+const url = process.env.VITE_DEV_SERVER_URL;
+const indexHtml = join(process.env.DIST, "index.html");
 const store = new Store();
 
 async function createWindow() {
   win = new BrowserWindow({
-    title: 'Godot Universal Project Manager',
-    icon: join(process.env.PUBLIC, 'favicon.ico'),
+    title: "Godot Universal Project Manager",
+    icon: join(process.env.PUBLIC, "favicon.ico"),
     width: 840,
     height: 600,
     webPreferences: {
@@ -58,95 +57,99 @@ async function createWindow() {
       nodeIntegration: true,
       contextIsolation: false,
     },
-  })
-  win.removeMenu()
+  });
+  win.removeMenu();
 
-  if (process.env.VITE_DEV_SERVER_URL) { // electron-vite-vue#298
-    win.loadURL(url)
+  if (process.env.VITE_DEV_SERVER_URL) {
+    // electron-vite-vue#298
+    win.loadURL(url);
     // Open devTool if the app is not packaged
-    win.webContents.openDevTools()
+    win.webContents.openDevTools();
   } else {
-    win.loadFile(indexHtml)
+    win.loadFile(indexHtml);
   }
 
+  console.log("I just sent an awesome statusbar name");
+
   // Test actively push message to the Electron-Renderer
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', new Date().toLocaleString())
-  })
+  win.webContents.on("did-finish-load", () => {
+    win?.webContents.send("main-process-message", new Date().toLocaleString());
+  });
 
   // Make all links open with the browser, not with the application
   win.webContents.setWindowOpenHandler(({ url }) => {
-    if (url.startsWith('https:')) shell.openExternal(url)
-    return { action: 'deny' }
-  }) 
+    if (url.startsWith("https:")) shell.openExternal(url);
+    return { action: "deny" };
+  });
 
   // setup ipc
-  ipcMain.on('download', (event, info) => {
-    console.log(app.getPath("appData"))
-    download(BrowserWindow.getFocusedWindow(), info.url, info.properties)
-      .then(dl => event.reply('download complete', dl.getSavePath()));
+  ipcMain.on("download", (event, info) => {
+    console.log(app.getPath("appData"));
+    download(BrowserWindow.getFocusedWindow(), info.url, info.properties).then(
+      (dl) => event.reply("download complete", dl.getSavePath())
+    );
   });
 
   // handle storing and getting settings
   let data: {
-    key: string,
-    value: any,
-  }
+    key: string;
+    value: any;
+  };
 
-  ipcMain.handle('store-setting', async (event, data) => {
-    console.log(data)
-    console.log(`${data.key} ${data.value}`)
-    store.set(data.key, data.value)
+  ipcMain.handle("store-setting", async (event, data) => {
+    console.log(data);
+    console.log(`${data.key} ${data.value}`);
+    store.set(data.key, data.value);
   });
 
-  ipcMain.handle('get-setting', async (event, key) => {
+  ipcMain.handle("get-setting", async (event, key) => {
     console.log(store.get(key));
   });
 }
 
 // handle requests to crawl the tuxfamily website
-ipcMain.handle('crawl-tuxfamily', async (event, url) => {
+ipcMain.handle("crawl-tuxfamily", async (event, url) => {
   // TODO
+  win.webContents.send("set-statusbar-name", "Awesome statusbar name!");
 });
 
+app.whenReady().then(createWindow);
 
-app.whenReady().then(createWindow)
+app.on("window-all-closed", () => {
+  win = null;
+  if (process.platform !== "darwin") app.quit();
+});
 
-app.on('window-all-closed', () => {
-  win = null
-  if (process.platform !== 'darwin') app.quit()
-})
-
-app.on('second-instance', () => {
+app.on("second-instance", () => {
   if (win) {
     // Focus on the main window if the user tried to open another
-    if (win.isMinimized()) win.restore()
-    win.focus()
+    if (win.isMinimized()) win.restore();
+    win.focus();
   }
-})
+});
 
-app.on('activate', () => {
-  const allWindows = BrowserWindow.getAllWindows()
+app.on("activate", () => {
+  const allWindows = BrowserWindow.getAllWindows();
   if (allWindows.length) {
-    allWindows[0].focus()
+    allWindows[0].focus();
   } else {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // New window example arg: new windows url
-ipcMain.handle('open-win', (_, arg) => {
+ipcMain.handle("open-win", (_, arg) => {
   const childWindow = new BrowserWindow({
     webPreferences: {
       preload,
       nodeIntegration: true,
       contextIsolation: false,
     },
-  })
+  });
 
   if (process.env.VITE_DEV_SERVER_URL) {
-    childWindow.loadURL(`${url}#${arg}`)
+    childWindow.loadURL(`${url}#${arg}`);
   } else {
-    childWindow.loadFile(indexHtml, { hash: arg })
+    childWindow.loadFile(indexHtml, { hash: arg });
   }
-})
+});
--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,16 +1,44 @@
+import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { ipcRenderer } from 'electron';
 import "./header.scss";
 import HeaderTab from './HeaderTab';
 
 export default function Header() {
+  const noStatusText = "No background process";
+
   const location = useLocation().pathname;
+  const [statusText, setStatusText] = useState(noStatusText);
+  const [satusIsRunning, setStatusIsRunning] = useState(false);
+
+
+
+  ipcRenderer.on('set-statusbar-name', (event, name: string) => {
+    if (name === "") {
+      setStatusText(noStatusText);
+      setStatusIsRunning(false);
+    }
+    else {
+      setStatusText(name);
+      setStatusIsRunning(true);
+    }
+  })
+
   return (
-    <header>
-      <div className="header-tabs">
-        <HeaderTab route={location} title="Projects" url="/" />
-        <HeaderTab route={location} title="Versions" url="/versions" />
-        <HeaderTab route={location} title="Config" url="/config" />
+    <>
+      <header>
+        <div className="header-tabs">
+          <HeaderTab route={location} title="Projects" url="/" />
+          <HeaderTab route={location} title="Versions" url="/versions" />
+          <HeaderTab route={location} title="Config" url="/config" />
+        </div>
+      </header>
+      <div className="status-bar">
+          <p>
+            <span id="statusLogo" className="status-bar-icon">Logo</span> 
+            <span>{statusText}</span>
+          </p>
       </div>
-    </header>
+    </>
   );
 }
